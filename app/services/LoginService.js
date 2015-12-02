c836ca33e875a18f539a98e3ef694b53234d9544
Maciej Sroka <mck159@gmail.com>
2015-12-02 22:59:14 +0100

    Changed login json

--- app/services/LoginService.js
+++ app/services/LoginService.js
@@ -1,6 +1,6 @@
 beaconsAdminApp.service('LoginService', ['$http', 'serverUri', function($http, invoicesServiceUri) {
 
     this.login = function (user, pass) {
-        return $http.post(invoicesServiceUri.replace('api/', '') + 'auth', {user: user, pass: pass}, {withCredentials: true});
+        return $http.post(invoicesServiceUri.replace('api/', '') + 'auth', {login: user, pass: pass}, {withCredentials: true});
     }
 }]);
