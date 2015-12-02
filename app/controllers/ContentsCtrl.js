999e7cd134f62359c39415080195dfa399f4ce71
Maciej Sroka <mck159@gmail.com>
2015-12-02 21:44:54 +0100

    Content refresh fix

--- app/controllers/ContentsCtrl.js
+++ app/controllers/ContentsCtrl.js
@@ -1,33 +1,60 @@
 beaconsAdminApp.controller('ContentsCtrl', ['$scope', '$resource', '$state', 'serverUri', '$http', 'Page', function ContentsCtrl($scope, $resource, $state, serverUri, $http, Page) {
     Page.setTitle("Contents");
     var ContentsResource = $resource(serverUri + 'content/:id',{id: "@_id"}, {update: {method : 'PUT'}});
-    var contents = ContentsResource.query(function() {
-        $scope.contents = contents;
-    });
-    $scope.deleteContent = function(content) {
-        content.$delete({id: content.content_id});
+
+
+    $scope.getContents = function() {
         var contents = ContentsResource.query(function() {
             $scope.contents = contents;
         });
     }
 
+    $scope.getContents();
+    $scope.deleteContent = function(content) {
+        content.$delete({id: content.content_id}).then(
+            function(data) {
+                $scope.getContents();
+            }
+        );
+        var contents = ContentsResource.query(function() {
+            $scope.contents = contents;
+        });
+        $scope.getContents();
+    }
+
     $scope.saveContent = function(content) {
         delete  content.editable;
-        content.$update({id: content.content_id}, content, function(data) {console.log(data)});
+        content.$update({id: content.content_id}, content).then(
+            function(data) {
+                $scope.getContents();
+            }
+        );
+
+    }
+
+    $scope.cancelEdit = function() {
+        $scope.getContents();
     }
 
     $scope.addContent = function() {
         var c = new ContentsResource();
         c.editable = true;
         c.new = true;
-        $scope.contents.push(c)
-
+        $scope.contents.push(c);
     }
 
     $scope.newContent = function(content) {
         delete  content.editable;
         delete  content.new;
         content.user_id = 1;
-        content.$save(content);
+        content.$save(content).then(
+            function(data) {
+                $scope.getContents();
+            },
+            function(error) {
+                alert("Invalid data provided");
+            }
+        );
+
     }
 }]);
